fcd7ff91a5d7b275fa1d701cb3a57ec37f1fe12d
BrunoLoFaro <lofarobruno@gmail.com>
2022-09-13 16:25:10 -0300

    AddedAFIPModels

---
+++ PracticaFinal_ModuloI/BusinessModel/Modelos/CodActividad.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessModel.Modelos
+{
+    public class CodActividad
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+        public string Codigo { get; set; }
+    }
+}
---
+++ PracticaFinal_ModuloI/BusinessModel/Modelos/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessModel.Modelos
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public int CodActividadId { get; set; }
+        public CodActividad CodActividad { get; set; }
+        public string ClaveTributaria { get; set; }
+    }
+}
