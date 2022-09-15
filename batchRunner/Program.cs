c2ac44ec589e3c81615b95fe2b68edf6b039038a
LoFaroBruno <110917929+LoFaroBruno@users.noreply.github.com>
2022-09-15 16:47:30 -0300

    BatchRunner

---
+++ batchRunner/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace batchRunner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                LaunchCommandLineApp();
+        }
+        static void LaunchCommandLineApp()
+        {
+            const string param1 = "../../transferencias.txt";
+            const string param2 = "../../transferencias_completo.txt";
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "../../../CompletadorDeTransferencias/bin/Debug/CompletadorDeTransferencias.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = "-i" + " " + param1 + " " + "-o" + " " + param2;
+
+            try
+            {
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                    if (exeProcess.ExitCode == 0)
+                        Console.WriteLine("The program executed successfully.");
+                    else
+                        Console.WriteLine($"The program exited with status: {exeProcess.ExitCode}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"{ex} {ex?.InnerException} {ex.StackTrace}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
